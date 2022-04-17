f12ee67740b9b4aafdde72dcd0e5a15920bb4d1e
bond1999 <46317327+bond1999@users.noreply.github.com>
2022-04-16 22:27:32 -0500

    Update v0.5: Message to be sent parsed

--- client/client.py
+++ client/client.py
@@ -5,7 +5,7 @@ import os
 
 SERVER_IP = "127.0.0.1"
 PORT = 8008
-
+ID = ""
 
 # Assigning ClientID
 def AssignID():
@@ -13,8 +13,6 @@ def AssignID():
     f2 = open("usedClientIDs.txt", "r+")
     IDs = f1.readlines()
     usedIDs = f2.readlines()
-    ID = ""
-
     assigned = 0
     for used in IDs:
         if used not in usedIDs:
@@ -22,13 +20,25 @@ def AssignID():
             ID = used
             ID = ID[0:-2]
             f2.write(used)
-            break
+            return ID
         assigned = 0
     f1.close()
     f2.close()
 
     if assigned == 0:
         print("Could not assign ID, too many users! Please try again later. No free lunch in Life :)\n")
+        return "InvalidUser"
+
+def Parse(MESSAGE):
+    print(MESSAGE)
+    print(type(MESSAGE))
+    print(len(MESSAGE))
+
+    if MESSAGE.lower() == "log on":
+        return f"HELLO({ID})\n"                                   # ASK THE TA!!!
+    if MESSAGE.lower() == "log off":
+        return f"InsertFunctionHere({ID})\n"                                   # ASK THE TA!!!
+
 
 # Generate Random Client IDs (10 character strings)
 # i=0
@@ -42,40 +52,26 @@ def AssignID():
 #     f2.write(ID)
 #     i += 1
 
-def LogOn():
-    f1 = open("clientsIDs.txt", "a+")
-    ID = f1.readline()
-    if not ID:
-        print("No clients registered, registering client!")
-        letters = string.ascii_lowercase
-        ID = ''.join(random.choice(letters) for i in range(10))
-        f2 = open("../server/listofsubscribers.txt", "a")
-        f1.write(ID)
-        f2.write(ID)
-        HELLO(ID)
-    else:
-        HELLO(ID)
-
 def HELLO(ClientID):
-    f1 = open("clientsIDs.txt", "a+")
-    ID = f1.readline()
-    if not ID:
-        print("No clients registered, registering client!")
-        letters = string.ascii_lowercase
-        ID = ''.join(random.choice(letters) for i in range(10))
-        f2 = open("../server/listofsubscribers.txt", "a")
-        f1.write(ID)
-        f2.write(ID)
+    UDPClientSocket.sendto(bytesToSend, serverAddressPort)
 
 def main():
-    AssignIDs()
+    global ID
+    ID = AssignID()
 
-    MESSAGE = bytes(input(f"{ID} > "), 'UTF-8')
-    # print("UDP target IP: %s" % SERVER_IP)
-    # print("UDP target port: %s" % PORT)
-    print("%s" % str(MESSAGE, 'utf-8'))
+    # Opening a Socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP Connection to the Internet
 
-    # sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP Connection to the Internet
-    # sock.sendto(MESSAGE, (SERVER_IP, PORT))
+    # Parsed Input is sent as a message through the UDP Socket
+    flag = 1
+    while True:
+
+        MESSAGE = bytes(Parse(input(f"{ID} > ")), "utf-8")
+        sock.sendto(MESSAGE, (SERVER_IP, PORT))
+        # print("UDP target IP: %s" % SERVER_IP)
+        # print("UDP target port: %s" % PORT)
+        print("%s" % str(MESSAGE, 'utf-8'))
+
+    print("Thank you for participating in our chat bot!\n")
 
 main()
