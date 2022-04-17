6446206bacafb7dc6b98183aa95e3ef208b05117
bond1999 <46317327+bond1999@users.noreply.github.com>
2022-04-16 21:48:26 -0500

    Update v0.4: Refactoring

--- client/client.py
+++ client/client.py
@@ -8,42 +8,29 @@ PORT = 8008
 
 
 # Assigning ClientID
-f1 = open("clientsIDs.txt", "r")
-f2 = open("usedClientIDs.txt", "r+")
-IDs = f1.readlines()
-usedIDs = f2.readlines()
-ID = ""
+def AssignID():
+    f1 = open("clientsIDs.txt", "r")
+    f2 = open("usedClientIDs.txt", "r+")
+    IDs = f1.readlines()
+    usedIDs = f2.readlines()
+    ID = ""
 
-print(f1)
-print(f2)
-
-assigned = 0
-for used in IDs:
-    if used not in usedIDs:
-        assigned = 1
-        ID = used
-        ID = ID[0:-2]
-        f2.write(used)
-        break
     assigned = 0
-f1.close()
-f2.close()
+    for used in IDs:
+        if used not in usedIDs:
+            assigned = 1
+            ID = used
+            ID = ID[0:-2]
+            f2.write(used)
+            break
+        assigned = 0
+    f1.close()
+    f2.close()
 
-if assigned == 0:
-    print("Could not assign ID, too many users! Please try again later. No free lunch in Life :)\n")
+    if assigned == 0:
+        print("Could not assign ID, too many users! Please try again later. No free lunch in Life :)\n")
 
-# MESSAGE = bytes(f"{ID} > ", 'UTF-8')
-
-MESSAGE = bytes(input(f"{ID} > "), 'UTF-8')
-# print("UDP target IP: %s" % SERVER_IP)
-# print("UDP target port: %s" % PORT)
-print("%s" % str(MESSAGE, 'utf-8'))
-
-# sock = socket.socket(socket.AF_INET, # Internet
-#                      socket.SOCK_DGRAM) # UDP
-# sock.sendto(MESSAGE, (SERVER_IP, PORT))
-
-# Generate Random Client IDs (10 character strings
+# Generate Random Client IDs (10 character strings)
 # i=0
 # while i < 10:
 #     letters = string.ascii_lowercase
@@ -79,3 +66,16 @@ def HELLO(ClientID):
         f2 = open("../server/listofsubscribers.txt", "a")
         f1.write(ID)
         f2.write(ID)
+
+def main():
+    AssignIDs()
+
+    MESSAGE = bytes(input(f"{ID} > "), 'UTF-8')
+    # print("UDP target IP: %s" % SERVER_IP)
+    # print("UDP target port: %s" % PORT)
+    print("%s" % str(MESSAGE, 'utf-8'))
+
+    # sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP Connection to the Internet
+    # sock.sendto(MESSAGE, (SERVER_IP, PORT))
+
+main()
