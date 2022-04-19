26ab0918363e577f1822d764b1c31ede1d908a30
bond1999 <46317327+bond1999@users.noreply.github.com>
2022-04-19 15:37:18 -0500

    Comments + Refactoring

--- client/client.py
+++ client/client.py
@@ -9,19 +9,22 @@ SERVER_IP = "127.0.0.1"
 PORT = 8008
 ID = ""
 
-# Assigning ClientID
+# Function to assign each client an ID which is not part of usedClientIDs (currently active clients)
 def AssignID():
+    # Each client is assigned a pre-selected unique 10-character string as an ID
+    # While a client is "active" (has not logged-off), their IDs are
+    # stored in client/usedClientIDs.txt for unique assignment of IDs
     f1 = open("clientsIDs.txt", "r")
     f2 = open("usedClientIDs.txt", "r+")
     IDs = f1.readlines()
     usedIDs = f2.readlines()
     assigned = 0
-    for used in IDs:
-        if used not in usedIDs:
+    for newID in IDs:
+        if newID not in usedIDs:
             assigned = 1
-            ID = used
+            ID = newID
             ID = ID[0:-2]
-            f2.write(used)
+            f2.write(newID)
             return ID
         assigned = 0
     f1.close()
@@ -31,36 +34,56 @@ def AssignID():
         print("Could not assign ID, too many users! Please try again later. No free lunch in Life :)")
         return "InvalidUser"
 
+# Function to print server timeout response incase server takes too long to respond
+def timeout():
+    print("Server did not respond, timed out... Try re-logging again.")
+
+# Function to parse each message input by the client to do respective functions
 def parse(MESSAGE):
+    # Match case to non-character sensitive message
     match MESSAGE.lower():
         case "log on":
+            print("Please wait while we are trying to establish a connection to the chat server...")
             authorize()
         case "log off":
             print("Thank you for participating in our chat bot!")
             exit(0)
     return MESSAGE
 
-def timeout():
-    print("Server did not respond, timed out... Try re-logging again.")
-
-
-
+# Function to Authorize client on typing "log on"
 def authorize():
-    timeout_timer = Timer(4, timeout)  # Call function timeout() in 60 seconds, 4 seconds for testing
-    CHALLENGE_RECEIVED = 1
-
-    # Sending Hello
+    challenge_timeout = Timer(4, timeout)  # Call function timeout() in 60 seconds, 4 seconds for testing
+    response_timeout = Timer(4, timeout)  # Call function timeout() in 60 seconds, 4 seconds for testing
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP Connection to the Internet
+    CHALLENGE_RECEIVED = 1
+    AUTH_SUCCESS = 0
+    AUTH_FAIL = 1
+    # Sending HELLO(Client-ID) to server
+    print("Connection established! Attempting Handshake...")
     sock.sendto(bytes(f"HELLO({ID})", 'utf-8'), (SERVER_IP, PORT))
 
-    # Waiting for a Challenge
-    timeout_timer.start()
-    time.sleep(5)                       # For Testing : Waiting for Terrorists to win.
+    # Waiting for CHALLENGE(rand) from server
+    challenge_timeout.start()
+    # INTENTIONAL FAIL
+    time.sleep(5)                       # For Testing : Waiting for Terrorists to win, comment-out otherwise
     if CHALLENGE_RECEIVED:
-        timeout_timer.cancel()          # Bomb has been defused
-        #Send Response
+        # Bomb has been Defused
+        challenge_timeout.cancel()
 
-    # Do nothing so input goes back to main for client to re-try login.
+        # Sending RESPONSE(Client-ID, Res) to server
+        print("Handshake established! Authenticating User...")
+        # Insert Code Here
+
+        # Waiting for CHALLENGE(rand) from server
+        response_timeout.start()
+        # INTENTIONAL FAIL
+        time.sleep(5)  # For Testing : Waiting for Terrorists to win, comment-out otherwise
+        if AUTH_SUCCESS:
+            response_timeout.cancel()
+        elif AUTH_FAIL:
+            response_timeout.cancel()
+
+    # Do nothing so input goes back to main for client to re-try login
 
 # Generate Random Client IDs (10 character strings)
 # i=0
@@ -74,25 +97,22 @@ def authorize():
 #     f2.write(ID)
 #     i += 1
 
+# Main Function to drive the program
 def main():
+    # Assigning unique ID to client
     global ID
     ID = AssignID()
 
     # Opening a Socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP Connection to the Internet
 
-    # Parsed Input is sent as a message through the UDP Socket
-    flag = 1
+    # Loop to parse through each message from the client
     while True:
-
         MESSAGE = bytes(parse(input(f"{ID} > ")), 'utf-8')
         sock.sendto(MESSAGE, (SERVER_IP, PORT))
         # REPLY = sock.recvfrom(1024)
-        # print("UDP target IP: %s" % SERVER_IP)
-        # print("UDP target port: %s" % PORT)
         print("MESSAGE : %s\n" % str(MESSAGE, 'utf-8'))
         # print("REPLY : %s\n" % str(REPLY, 'utf-8'))
 
-    print("Thank you for participating in our chat bot!\n")
 
 main()
