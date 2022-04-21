84f328d490257f37ad9ec573739a19b2e616f7aa
Timothy Choi <timothychoi14@gmail.com>
2022-04-21 13:08:19 -0500

    worked on auth

--- client/client.py
+++ client/client.py
@@ -1,11 +1,14 @@
 import socket
+import time
+from threading import Timer
 import string
 import random
 import os
 
 SERVER_IP = "127.0.0.1"
-PORT = 8008
+PORT = 8009
 ID = ""
+
 
 # Assigning ClientID
 def AssignID():
@@ -27,51 +30,75 @@ def AssignID():
 
     if assigned == 0:
         print("Could not assign ID, too many users! Please try again later. No free lunch in Life :)\n")
-        return "InvalidUser"
+        return "InvalidUser"  # can still type ID is just set to invalid user
 
+
+# Function to print server timeout response in case server takes too long to responnd
+def timeout():
+    print("Server did not respond, Timed out... Try re-logging again")
+
+
+# Function to parse each message input bu the clien to do respective functions
 def Parse(MESSAGE):
-    print(MESSAGE)
-    print(type(MESSAGE))
-    print(len(MESSAGE))
+    match MESSAGE.lower():
+        case "log on":
+            print("Please wait shile we are trying to establish a connection to the chat server...")
+            authorize()
+        case "log off":
+            print("Thank you for participating in our chat bot")
+            exit(0)
+    return f"{ID}{MESSAGE}"
 
-    if MESSAGE.lower() == "log on":
-        return f"HELLO({ID})\n"                                   # ASK THE TA!!!
-    if MESSAGE.lower() == "log off":
-        return f"InsertFunctionHere({ID})\n"                                   # ASK THE TA!!!
+def authorize():
+    challenge_timeout = Timer(60, timeout)  # Call function timeout() in 60 seconds, 4 seconds for testing
+    response_timeout = Timer(60, timeout)  # Call function timeout() in 60 seconds, 4 seconds for testing
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP Connection to the Internet
+    CHALLENGE_RECEIVED = 1
+    AUTH_SUCCESS = 0
+    AUTH_FAIL = 1
+    # Sending HELLO(Client-ID) to server
+    print("Connection established! Attempting Handshake...")
+    sock.sendto(bytes(f"HELLO({ID})", 'utf-8'), (SERVER_IP, PORT))
 
+    # Waiting for CHALLENGE(rand) from server
+    challenge_timeout.start()
+    # INTENTIONAL FAIL
+    time.sleep(5)  # For Testing : Waiting for Terrorists to win, comment-out otherwise
+    if CHALLENGE_RECEIVED:
+        # Bomb has been Defused
+        challenge_timeout.cancel()
 
-# Generate Random Client IDs (10 character strings)
-# i=0
-# while i < 10:
-#     letters = string.ascii_lowercase
-#     ID = ''.join(random.choice(letters) for i in range(10))
-#     ID = ID + "\n"
-#     f2 = open("server/listofsubscribers.txt", "a")
-#     f1 = open("client/clientsIDs.txt", "a")
-#     f1.write(ID)
-#     f2.write(ID)
-#     i += 1
+        # Sending RESPONSE(Client-ID, Res) to server
+        print("Handshake established! Authenticating User...")
+        # Insert Code Here
 
-def HELLO(ClientID):
-    UDPClientSocket.sendto(bytesToSend, serverAddressPort)
+        # Waiting for CHALLENGE(rand) from server
+        response_timeout.start()
+        # INTENTIONAL FAIL
+        time.sleep(5)  # For Testing : Waiting for Terrorists to win, comment-out otherwise
+        if AUTH_SUCCESS:
+            response_timeout.cancel()
+        elif AUTH_FAIL:
+            response_timeout.cancel()
 
 def main():
     global ID
     ID = AssignID()
 
     # Opening a Socket
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP Connection to the Internet
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP Connection to the Internet
 
     # Parsed Input is sent as a message through the UDP Socket
-    flag = 1
     while True:
-
         MESSAGE = bytes(Parse(input(f"{ID} > ")), "utf-8")
         sock.sendto(MESSAGE, (SERVER_IP, PORT))
+        #REPLY = sock.recvfrom(1024)
         # print("UDP target IP: %s" % SERVER_IP)
         # print("UDP target port: %s" % PORT)
-        print("%s" % str(MESSAGE, 'utf-8'))
+        #print("%s" % str(MESSAGE, 'utf-8'))
+        print("REPLY : %s\n" % str(MESSAGE, 'utf-8'))
 
     print("Thank you for participating in our chat bot!\n")
+
 
 main()
--- server/server.py
+++ server/server.py
@@ -1,15 +1,78 @@
 import socket
+import string
+import datetime
+import random
+import secrets
+#import chat_history
+import threading
+x = datetime.datetime.now()
 
 HOST_IP = "127.0.0.1"
-PORT = 8008
+PORT = 8009
+
 
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 sock.bind((HOST_IP, PORT))
 
+def connect(MESSAGE):
+    print(MESSAGE)
+
+#def chat_started(clients, payload):
+#    session_ID = 1234   #redefine how this session ID is created
+#    curr_time = x.strftime("%x\t\t%I:%M:%S%p")
+#    return chat_history.access_log(curr_time, session_ID, clients, payload)
+def a3(key, rand):
+    k = key
+    m = random.getrandbits(128)
+    kb = bin(k)[6:]
+    mb = bin(m)[4:]
+    kbl = kb[0:64]
+    kbr = kb[64:]
+    mbl = mb[0:64]
+    mbr = mb[64:]
+    a1 = int(kbl, 2)
+    int(mbr, 2)
+    a2 = int(kbr, 2)
+    int(mbl, 2)
+    a3 = a1 ^ a2
+    a4 = bin(a3)[2:].zfill(64)
+    a5 = a4[0:32]
+    a6 = a4[32:]
+    a7 = int(a5, 2)
+    int(a6, 2)
+
+def K_Agen():
+    i = 0
+    while i < 10:
+        n = random.getrandbits(128)
+        n = n + "\n"
+        f1 = open("server/sK_A.txt", "a")
+        f2 = open("client/cK_A.txt", "a")
+        f1.write(n)
+        f2.write(n)
+        i += 1
+
+def rand_num():
+    nums = string.digits
+    nu = ''.join(random.choice(nums) for i in range(10))
+    print(nu)
+    K_Agen()
+    challenge(nu)
+
+
+def challenge(rand):
+    sock.sendto(bytes(rand, "utf-8"), ((HOST_IP, PORT)))
+
+
+print("[STARTING] server is starting...")
+#start()
 while True:
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
     print("received message: %s" % data)
+    if "HELLO(" in str(data.decode('utf-8')):
+        rand_num()
+
     # 1. Check if client is on list of subscribers
     # 2. Retrieve the client’s secret key and send a CHALLENGE (rand) message to the client, using UDP
     # 3. Wait for RESPONSE from client.
@@ -20,3 +83,4 @@ while True:
     # From this point on, until the TCP connection is closed, all data (signaling messages and chat) is exchanged over the TCP connection.
     # 6. The server sends CONNECTED to the client. The client is connected.
     # 7. If client types “Log off” or when the activity timer expires, the TCP connection is closed.
+
