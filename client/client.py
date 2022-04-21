9255d387f5350414b1367c14a26c9b25b85c9a3e
bond1999 <46317327+bond1999@users.noreply.github.com>
2022-04-21 14:38:00 -0500

    rolledback

--- client/client.py
+++ client/client.py
@@ -6,58 +6,73 @@ import random
 import os
 
 SERVER_IP = "127.0.0.1"
-PORT = 8008
+PORT = 8009
 ID = ""
 
-# Function to assign each client an ID which is not part of usedClientIDs (currently active clients)
+
+# Assigning ClientID
 def AssignID():
-    # Each client is assigned a pre-selected unique 10-character string as an ID
-    # While a client is "active" (has not logged-off), their IDs are
-    # stored in client/usedClientIDs.txt for unique assignment of IDs
     f1 = open("clientsIDs.txt", "r")
     f2 = open("usedClientIDs.txt", "r+")
     IDs = f1.readlines()
     usedIDs = f2.readlines()
     assigned = 0
-    for newID in IDs:
-        if newID not in usedIDs:
+    for used in IDs:
+        if used not in usedIDs:
             assigned = 1
-            ID = newID
+            ID = used
             ID = ID[0:-2]
-            f2.write(newID)
+            f2.write(used)
             return ID
         assigned = 0
     f1.close()
     f2.close()
 
     if assigned == 0:
-        print("Could not assign ID, too many users! Please try again later. No free lunch in Life :)")
-        return "InvalidUser"
+        print("Could not assign ID, too many users! Please try again later. No free lunch in Life :)\n")
+        return "InvalidUser"  # can still type ID is just set to invalid user
 
-# Function to print server timeout response incase server takes too long to respond
+#assign key
+def AssignK():
+    f1 = open("cK_A.txt", "r")
+    f2 = open("usedcK_A.txt", "r+")
+    k = f1.readlines()
+    usedk = f2.readlines()
+    assigned = 0
+    for used in k:
+        if used not in usedk:
+            assigned = 1
+            k = used
+            k = k[0:-2]
+            f2.write(used)
+            return k
+        assigned = 0
+    f1.close()
+    f2.close()
+
+# Function to print server timeout response in case server takes too long to responnd
 def timeout():
-    print("Server did not respond, timed out... Try re-logging again.")
+    print("Server did not respond, Timed out... Try re-logging again")
 
-# Function to parse each message input by the client to do respective functions
-def parse(MESSAGE):
-    # Match case to non-character sensitive message
+
+# Function to parse each message input bu the clien to do respective functions
+def Parse(MESSAGE):
     match MESSAGE.lower():
         case "log on":
-            print("Please wait while we are trying to establish a connection to the chat server...")
+            print("Please wait shile we are trying to establish a connection to the chat server...")
             authorize()
         case "log off":
-            print("Thank you for participating in our chat bot!")
+            print("Thank you for participating in our chat bot")
             exit(0)
-    return f"{MESSAGE}"
+    return f"{ID}{MESSAGE}"
 
-# Function to Authorize client on typing "log on"
 def authorize():
-    challenge_timeout = Timer(4, timeout)  # Call function timeout() in 60 seconds, 4 seconds for testing
-    response_timeout = Timer(4, timeout)  # Call function timeout() in 60 seconds, 4 seconds for testing
+    challenge_timeout = Timer(60, timeout)  # Call function timeout() in 60 seconds, 4 seconds for testing
+    response_timeout = Timer(60, timeout)  # Call function timeout() in 60 seconds, 4 seconds for testing
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP Connection to the Internet
     CHALLENGE_RECEIVED = 1
-    AUTH_SUCCESS = 1
-    AUTH_FAIL = 0
+    AUTH_SUCCESS = 0
+    AUTH_FAIL = 1
     # Sending HELLO(Client-ID) to server
     print("Connection established! Attempting Handshake...")
     sock.sendto(bytes(f"HELLO({ID})", 'utf-8'), (SERVER_IP, PORT))
@@ -65,55 +80,46 @@ def authorize():
     # Waiting for CHALLENGE(rand) from server
     challenge_timeout.start()
     # INTENTIONAL FAIL
-    # time.sleep(5)                       # For Testing : Waiting for Terrorists to win, comment-out otherwise
-    if CHALLENGE_RECEIVED and challenge_timeout.is_alive():
+    time.sleep(5)  # For Testing : Waiting for Terrorists to win, comment-out otherwise
+    if CHALLENGE_RECEIVED:
         # Bomb has been Defused
         challenge_timeout.cancel()
 
         # Sending RESPONSE(Client-ID, Res) to server
         print("Handshake established! Authenticating User...")
-
         # Insert Code Here
 
         # Waiting for CHALLENGE(rand) from server
         response_timeout.start()
         # INTENTIONAL FAIL
-        # time.sleep(5)  # For Testing : Waiting for Terrorists to win, comment-out otherwise
-        if AUTH_SUCCESS and response_timeout.is_alive():
+        time.sleep(5)  # For Testing : Waiting for Terrorists to win, comment-out otherwise
+        if AUTH_SUCCESS:
             response_timeout.cancel()
-        elif AUTH_FAIL and response_timeout.is_alive():
+        elif AUTH_FAIL:
             response_timeout.cancel()
 
-    # Do nothing so input goes back to main for client to re-try login
-
-# Generate Random Client IDs (10 character strings)
-# i=0
-# while i < 10:
-#     letters = string.ascii_lowercase
-#     ID = ''.join(random.choice(letters) for i in range(10))
-#     ID = ID + "\n"
-#     f2 = open("server/listofsubscribers.txt", "a")
-#     f1 = open("client/clientsIDs.txt", "a")
-#     f1.write(ID)
-#     f2.write(ID)
-#     i += 1
-
-# Main Function to drive the program
 def main():
-    # Assigning unique ID to client
     global ID
+    global K
     ID = AssignID()
+    K = AssignK()
+    print(ID)
+    print(K)
 
     # Opening a Socket
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP Connection to the Internet
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP Connection to the Internet
 
-    # Loop to parse through each message from the client
+    # Parsed Input is sent as a message through the UDP Socket
     while True:
-        MESSAGE = bytes(parse(input(f"{ID} > ")), 'utf-8')
-        # sock.sendto(MESSAGE, (SERVER_IP, PORT))
-        # REPLY = sock.recvfrom(1024)
-        print("MESSAGE : %s\n" % str(MESSAGE, 'utf-8'))
-        # print("REPLY : %s\n" % str(REPLY, 'utf-8'))
+        MESSAGE = bytes(Parse(input(f"{ID} > ")), "utf-8")
+        sock.sendto(MESSAGE, (SERVER_IP, PORT))
+        #REPLY = sock.recvfrom(1024)
+        # print("UDP target IP: %s" % SERVER_IP)
+        # print("UDP target port: %s" % PORT)
+        #print("%s" % str(MESSAGE, 'utf-8'))
+        print("REPLY : %s\n" % str(MESSAGE, 'utf-8'))
+
+    print("Thank you for participating in our chat bot!\n")
 
 
 main()
--- server/server.py
+++ server/server.py
@@ -1,98 +1,92 @@
 import socket
-import chat_history
+import string
 import datetime
-x = datetime.datetime.now()
-
+import random
+import secrets
+import chat_history
+import chat_started
 HOST_IP = "127.0.0.1"
-PORT = 8008
+PORT = 8009
+
 
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 sock.bind((HOST_IP, PORT))
 
+# respond to user who types log on
+def connect(MESSAGE):
+    # send message to user tcp or udp?
+    print(MESSAGE)
 
-def challenge():
-    pass
+#def chat_started(clients, payload):
+#    session_ID = 1234   #redefine how this session ID is created
+#    curr_time = x.strftime("%x\t\t%I:%M:%S%p")
+#    return chat_history.access_log(curr_time, session_ID, clients, payload)
+def a3(key, rand):
+    k = key
+    m = random.getrandbits(128)
+    kb = bin(k)[6:]
+    mb = bin(m)[4:]
+    kbl = kb[0:64]
+    kbr = kb[64:]
+    mbl = mb[0:64]
+    mbr = mb[64:]
+    a1 = int(kbl, 2)
+    int(mbr, 2)
+    a2 = int(kbr, 2)
+    int(mbl, 2)
+    a3 = a1 ^ a2
+    a4 = bin(a3)[2:].zfill(64)
+    a5 = a4[0:32]
+    a6 = a4[32:]
+    a7 = int(a5, 2)
+    int(a6, 2)
+
+def rand_num():
+    nums = secrets.token_hex(8)
+    print(nums)
+    return nums
+    #a3()
+    #challenge(nums)
 
 
-def authenticate():
-    pass
+def challenge(rand):
+    sock.sendto(bytes(rand, "utf-8"), ((HOST_IP, PORT)))
 
 
-def connected():
-    pass
+print("[STARTING] server is starting...")
+#start()
+while True:
+    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+    print("SERVER-received message: %s" % data)
+    if "HELLO(" in str(data):
+        rand = rand_num()
+        XRES = a3()
+        challenge(rand)
+    #  sliced message to separate ID
+    client_A_ID = str(data[:9], 'utf-8')
+
+    # update client_B_ID--------------------------------
+    client_B_ID = 'client789'
+    print("client-A-ID is: ", client_A_ID)
+    print("client-B-ID is: ", client_B_ID)
+
+    #  sliced message to separate payload
+    payload = str(data[9:], 'utf-8')
+    print("payload is: ", payload)
+    if payload[:7].lower().strip() == 'history':
+        print(payload[7:].lower().strip())
+        chat_history.read_log(client_A_ID, payload[7:].lower().strip())
+
+    #  sliced message to separate payload
+    if payload[:6].lower() == 'log on':
+        print("initiate log on")
+        connect("CONNECTED")
+    clients= [client_A_ID, client_B_ID]
+    res = chat_started(clients, payload)
+    print(res)
 
 
-def unavailable():
-    pass
-
-
-def end_notif():
-    pass
-
-# Function to parse client messages based on message sent by a certain client.
-def parse(MESSAGE, client_available=None):
-    if MESSAGE[0:5] == "HELLO(":
-        print(MESSAGE)
-        challenge()
-    if MESSAGE[0:8] == "RESPONSE(":
-        print(MESSAGE)
-        authenticate()
-    if MESSAGE[0:7] == "CONNECT(":
-        print(MESSAGE)
-        connected()
-    # S:Think if to include Client A connected messaged should be parsed through this or not
-    if MESSAGE[0:12] == "CHAT_REQUEST(":
-        if client_available:
-            print(MESSAGE)
-            chat_started()
-        elif not client_available:
-            print(MESSAGE)
-            unavailable()
-    if MESSAGE[0:11] == "END_REQUEST(":
-        print(MESSAGE)
-        end_notif()
-    if MESSAGE[0:4] == "CHAT(":
-        print(MESSAGE)
-        print("chat_history.write_log()")
-    if MESSAGE[0:11] == "HISTORY_REQ(":
-        print(MESSAGE)
-        chat_history.read_log("abcd", "efgh")
-
-# after chat is started between client A and client B
-def chat_started(clients, payload):
-    session_ID = 1234  # redefine how this session ID is created
-    curr_time = x.strftime("%x\t\t%I:%M:%S%p")
-    return chat_history.access_log(curr_time, session_ID, clients, payload)
-
-def main():
-    while True:
-        data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-        print("SERVER-received message: %s" % data)
-        #  sliced message to separate ID
-        client_A_ID = str(data[:9], 'utf-8')
-
-        # update client_B_ID--------------------------------
-        client_B_ID = 'client789'
-        print("client-A-ID is: ", client_A_ID)
-        print("client-B-ID is: ", client_B_ID)
-
-        #  sliced message to separate payload
-        payload = str(data[9:], 'utf-8')
-        print("payload is: ", payload)
-        if payload[:7].lower().strip() == 'history':
-            print(payload[7:].lower().strip())
-            chat_history.read_log(client_A_ID, payload[7:].lower().strip())
-
-        #  sliced message to separate payload
-        if payload[:6].lower() == 'log on':
-            print("initiate log on")
-            connect("CONNECTED")
-        clients= [client_A_ID, client_B_ID]
-        res = chat_started(clients, payload)
-        print(res)
-
-main()
 
 # 1. Check if client is on list of subscribers
 # 2. Retrieve the client’s secret key and send a CHALLENGE (rand) message to the client, using UDP
